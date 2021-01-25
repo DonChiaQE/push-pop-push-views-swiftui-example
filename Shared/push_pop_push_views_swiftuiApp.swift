fb7b1f68c8323e77c6ba1f12002b8d329b1cfe2e
lizardon <chia.qien.don@dhs.sg>
2021-01-25 14:09:36 +0800

    Initial Commit

---
+++ Shared/push_pop_push_views_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  push_pop_push_views_swiftuiApp.swift
+//  Shared
+//
+//  Created by Don Chia on 25/1/21.
+//
+
+import SwiftUI
+
+@main
+struct push_pop_push_views_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
